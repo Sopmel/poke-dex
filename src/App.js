d4d84761166f0deeff3159a9d794f02a798a4422
mazmann <sadi.lundberg@hotmail.com>
2023-11-24 13:40:16 +0100

    fixederrors

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ function App() {
 
   useEffect(() => {
     const fetchPokemon = async () => {
-      let pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-      let json = await pokemon.json();
-      setList();
-      //console.log(json);
+      try {
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        let data = await response.json();
+        setList(data.results);
+      } catch (error) {
+        console.error("Error fetching Pokemon:", error);
+      }
     };
+
     fetchPokemon();
-  });
+  }, []); // Pass an empty dependency array to ensure useEffect runs only once on mount
 
   return (
     <div className="App">
